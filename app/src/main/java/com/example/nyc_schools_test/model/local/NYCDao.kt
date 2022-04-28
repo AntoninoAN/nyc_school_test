d0f03819ac9912d93f0bf1d0a82d9e70c66126cc
Antonino AN <andonino@gmail.com>
2022-04-28 11:32:38 -0400

    Implemented room DB

---
+++ app/src/main/java/com/example/nyc_schools_test/model/local/NYCDao.kt
@@ -0,0 +1,16 @@
+package com.example.nyc_schools_test.model.local
+
+import androidx.room.Dao
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NYCDao {
+    @Update(entity = NycSchoolEntity::class,
+        onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(nycSchoolEntity: NycSchoolEntity)
+
+    @Query(value = "SELECT * FROM school_list")
+    suspend fun getListSchools(): List<NycSchoolEntity>
+}
